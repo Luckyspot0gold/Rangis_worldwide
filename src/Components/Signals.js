33ab9bfbc9ebb50f4dca29bdbbbc21b3ea836b99
Justin McCrea <mammerjam303@gmail.com>
2025-09-16 20:09:12 -0600

    Create Signals.js

---
+++ src/Components/Signals.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Signals() {
+  return (
+    <div className="signals">
+      <h2>Signals</h2>
+      <div className="signal-grid">
+        <div>HRS: ...</div>
+        <div>HSI: ...</div>
+        <div>ISS: ...</div>
+        <div>HIV: ...</div>
+        <div>SSS: ...</div>
+        <div>PSD: ...</div>
+      </div>
+    </div>
+  );
+}
+
+export default Signals;
