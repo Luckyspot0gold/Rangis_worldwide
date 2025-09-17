55d10912ddf1b8f190120e08ce7d391f0e8f7189
Justin McCrea <mammerjam303@gmail.com>
2025-09-17 11:07:39 -0600

    Create unified-accessbutton.js

---
+++ src/markets/xion/unified-accessbutton.js
@@ -0,0 +1,12 @@
+unified access to all asset classes
+const demonstrateXionValue = () => {
+  // NYSE stocks through Xion abstraction
+  playMarketSound('AAPL', 'stock')
+  
+  // FOREX through Xion abstraction  
+  playMarketSound('EURUSD', 'forex')
+  
+  // Crypto through Xion abstraction
+  playMarketSound('BTC', 'crypto')
+}
+```
