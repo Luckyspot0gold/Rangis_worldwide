1bce93096ca753f3a2ce5be3da0d2fb43424e7ea
Justin McCrea <mammerjam303@gmail.com>
2025-09-16 19:45:44 -0600

    Update webpack-config.js

--- Building_Blocks/webpack-config.js
+++ Building_Blocks/webpack-config.js
@@ -8,6 +8,19 @@ module.exports = {
   },
   mode: 'development',
   devServer: {
+    static: './',
+  },
+};
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
     static: {
       directory: path.join(__dirname, 'public'),
     },
