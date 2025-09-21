3166df2fe6b8cd27d4aff9410f995e0319af3efd
Justin McCrea <mammerjam303@gmail.com>
2025-09-20 21:57:44 -0600

    Update Local-hub.Next.js/config

--- Secrets/.env./Local-hub.Next.js
+++ Secrets/.env./Local-hub.Next.js
@@ -1,3 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
+
 NEXT_PUBLIC_TREASURY_ADDRESS="xion1svpts9q2ml4ahgc4tuu95w8cqzv988s6mf5mupt5kt56gvdnklks9hzar4"
 NEXT_PUBLIC_CONTRACT_ADDRESS="xion1aza0jdzfc7g0u64k8qcvcxfppll0cjeer56k38vpshe3p26q5kzswpywp9"
 NEXT_PUBLIC_RPC_URL="https://rpc.xion-testnet-2.burnt.com:443"
